59a2ac805decf4befc8529a8c2cc7a51c0679312
SaeRom Choi <127198819+fhazlt@users.noreply.github.com>
2023-08-19 12:55:21 +0900

    Delete backjoon directory

--- backjoon/D.S/2164.py
+++
@@ -1,6 +0,0 @@
-import sys
-n = int(sys.stdin.readline().strip())
-m = list(range(1,n+1))
-for i in range(n):
-    
-
--- backjoon/imple/10718.py
+++
@@ -1,6 +0,0 @@
-a = list()
-a = ['강한친구', '대한육군']
-for j in range(2):
-    for i in a:
-        print(i, end=' ')    
-    print()
