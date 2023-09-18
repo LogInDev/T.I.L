022758ca2c70ea2eac0f211373dcc99b26516808
fhazlt <fhazlt303@gmail.com>
2023-09-18 11:02:10 +0900

    solved BJ2292

---
+++ baekjoon/Math1/BJ2292.java
@@ -0,0 +1,30 @@
+package Math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2292 {
+
+	public static void main(String[] args) throws IOException{
+		//1 (+6*1) 7 (+6*2)  19 (+6*3) 37 (+6*4) 61
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int count =1;
+		int range =2;
+		if(N==1) {
+			System.out.println(1);
+		}else {
+			while(range < N) {
+				range = range + (6+count);
+				count++;
+			}
+			System.out.println(count);
+		}
+		
+		
+	}
+
+}
