e166bc0c86b004ea1941af6c3d51d1635357e9fb
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-02-19 12:33:40 +0900

    배열 4번 완료

--- javaAlgorithmTopic10/src/main/java/me/romkit/string/Step8.java
+++ javaAlgorithmTopic10/src/main/java/me/romkit/string/Step8.java
@@ -3,35 +3,7 @@ package me.romkit.string;
 import java.util.Scanner;
 
 public class Step8 {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String words = sc.nextLine();
-        reverseString(words);
 
-    }
-
-    static void reverseString(String words) {
-        char[] arr = words.toUpperCase().toCharArray();
-        int lt = 0;
-        int rt = arr.length - 1;
-        String result = "YES";
-        while (lt < rt) {
-            if (Character.isAlphabetic(words.charAt(lt)) && Character.isAlphabetic(words.charAt(rt))) {
-
-                if (arr[lt] != arr[rt]) {
-                    result = "NO";
-                    break;
-                }
-            }
-            lt++;
-            rt--;
-        }
-        System.out.println(result);
-
-    }
-}
-=======
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
