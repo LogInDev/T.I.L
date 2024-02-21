2143ce8e37715bad6e8ddda3a8227d115113080a
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-02-21 12:08:09 +0900

    배열 4번 손코딩 모드로 수정

--- javaAlgorithmTopic10/src/main/java/me/romkit/array/Step4.java
+++ javaAlgorithmTopic10/src/main/java/me/romkit/array/Step4.java
@@ -11,13 +11,21 @@ public class Step4 {
 
     }
     static void solution(int N, int[] arr){
-        arr[1] = 1;
+        /*arr[1] = 1;
         for(int i=0;i<N-1;i++){
             arr[i+2] = arr[i] + arr[i+1];
         }
         for(int i=1;i<N+1;i++) {
 
             System.out.print(arr[i]+" ");
+        }*/
+        int a=1, b=1, c;
+        System.out.println(a+" "+b+" ");
+        for(int i=2;i<N;i++){
+            c=a+b;
+            System.out.println(c + " ");
+            a = b;
+            b = c;
         }
     }
 }
