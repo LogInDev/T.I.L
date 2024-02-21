8584c3b8b636d822f6dee059f45f53fd5445552c
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-02-21 11:51:55 +0900

    배열 1번 수정

--- javaAlgorithmTopic10/src/main/java/me/romkit/array/Step1.java
+++ javaAlgorithmTopic10/src/main/java/me/romkit/array/Step1.java
@@ -9,15 +9,15 @@ public class Step1 {
         int N = sc.nextInt();
         sc.nextLine();
         String[] a = sc.nextLine().split(" ");
-        int[] arr = new int[N+1];
+        int[] arr = new int[N];
         for(int i=0;i<N;i++){
-            arr[i+1] = Integer.parseInt(a[i]);
+            arr[i] = Integer.parseInt(a[i]);
         }
         solution(N, arr);
 
     }
     static void solution(int N, int[] arr){
-        for(int i=1;i<arr.length;i++){
+        for(int i=1;i<N;i++){
             if(arr[i]>arr[i-1]){
                 System.out.print(arr[i] + " ");
             }
