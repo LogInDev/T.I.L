7a55fd3731b27f00b854c4eee5994fd0238b87f3
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-02-27 12:00:35 +0900

    배열 9번 풀이 upgrade

--- javaAlgorithmTopic10/src/main/java/me/romkit/array/Step9.java
+++ javaAlgorithmTopic10/src/main/java/me/romkit/array/Step9.java
@@ -18,32 +18,54 @@ public class Step9 {
 
     }
     static void solution(int N, int[][] arr){
-        int max=0;
-        // 가로행
-        for(int i=0;i<N;i++) {
-            int sum=0;
-            for(int j=0;j<N;j++){
-                sum+= arr[i][j];
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+        for(int i=0;i<N;i++){     // 행 번호
+            sum1 = sum2 = 0;
+            for(int j=0;j<N;j++){ // 열 번호
+                sum1 +=arr[i][j]; // 가로행 합
+                sum2 +=arr[j][i]; // 세로행 합
             }
-            if(sum>max) max=sum;
-            // 세로행
-            sum=0;
-            for(int j=0;j<N;j++){
-                sum+= arr[j][i];
-            }
-            if(sum>max) max=sum;
+            answer=Math.max(answer, sum1);
+            answer=Math.max(answer, sum2);
         }
-        // 대각행
-        int sum=0;
-        for(int j=0;j<N;j++){
-            sum+= arr[j][j];
+
+        sum1=sum2=0;
+        for(int i=0;i<N;i++){
+            sum1+=arr[i][i];
+            sum2+=arr[N-1-i][i];
         }
-        if(sum>max) max=sum;
-        sum=0;
-        for(int j=0;j<N;j++){
-            sum+= arr[j][N-1-j];
-        }
-        if(sum>max) max=sum;
-        System.out.println(max);
+        answer=Math.max(answer, sum1);
+        answer=Math.max(answer, sum2);
+        System.out.println(answer);
+
+//------------------------내 풀이---------
+//        int max=0;
+//        // 가로행
+//        for(int i=0;i<N;i++) {
+//            int sum=0;
+//            for(int j=0;j<N;j++){
+//                sum+= arr[i][j];
+//            }
+//            if(sum>max) max=sum;
+//            // 세로행
+//            sum=0;
+//            for(int j=0;j<N;j++){
+//                sum+= arr[j][i];
+//            }
+//            if(sum>max) max=sum;
+//        }
+//        // 대각행
+//        int sum=0;
+//        for(int j=0;j<N;j++){
+//            sum+= arr[j][j];
+//        }
+//        if(sum>max) max=sum;
+//        sum=0;
+//        for(int j=0;j<N;j++){
+//            sum+= arr[j][N-1-j];
+//        }
+//        if(sum>max) max=sum;
+//        System.out.println(max);
     }
 }
