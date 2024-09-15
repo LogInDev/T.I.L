1197d492a39dfdd098fcdffba49892d09a32e8dd
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-09-15 21:55:23 +0900

    P49994.방문 길이

--- algorithm/java/programmers/java-algorithm/src/arrEx/P49994.java
+++ algorithm/java/programmers/java-algorithm/src/arrEx/P49994.java
@@ -1,70 +1,58 @@
 package arrEx;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class P49994 {
     public int solution(String dirs) {
         int answer = 0;
-        //1. U, R, D, L을 숫자로 정의
-        int[][] d_load = {{0, -1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        //2. dirs를 하나씩 적용 -> count+1
-        String[] split_dirs = dirs.split("");
-        int cnt = 0;
+        // 현재 위치
+        int x = 0, y = 0;
 
-        int[][] load_arr = new int[split_dirs.length+1][2];
-        load_arr[0][0] = 0;
-        load_arr[0][1] = 0;
-        for (int i = 0; i < split_dirs.length; i++) {
-            int x = load_arr[i][0];
-            int y = load_arr[i][1];
-            switch (split_dirs[i]){
-                case "U" :
-                    x += d_load[0][0];
-                    y += d_load[0][1];
+        // 이동 경로를 저장할 Set
+        Set<String> answerSet = new HashSet<>();
+
+        for (char dir : dirs.toCharArray()) {
+            int newX = x, newY = y;
+
+            // 방향에 따라 새로운 위치 계산
+            switch (dir) {
+                case 'U':
+                    newY += 1;
                     break;
-                case "R" :
-                    x += d_load[1][0];
-                    y += d_load[1][1];
+                case 'R':
+                    newX += 1;
                     break;
-                case "D" :
-                    x += d_load[2][0];
-                    y += d_load[2][1];
+                case 'D':
+                    newY -= 1;
                     break;
-                case "L" :
-                    x += d_load[3][0];
-                    y += d_load[3][1];
+                case 'L':
+                    newX -= 1;
                     break;
             }
 
-            //2-1. 5 초과일 경우 5로, -5 미만일 경우 -5로 수정 -> count-1
-            if(x>5||x< -5||y>5||y< -5){
-                if (x >5) x = 5;
-                if(x< -5) x = -5;
-                if (y >5) y = 5;
-                if(y< -5) y = -5;
-                cnt++;
+            // 경계 체크
+            if (newX < -5 || newX > 5 || newY < -5 || newY > 5) {
+                continue; // 경계를 넘어가면 무시
             }
 
-            load_arr[i+1][0] = x;
-            load_arr[i+1][1] = y;
+            // 이동 경로를 문자열로 저장 (양방향)
+            String path1 = x + "," + y + "->" + newX + "," + newY;
+            String path2 = newX + "," + newY + "->" + x + "," + y;
 
+            // 경로가 이미 방문한 경로인지 체크
+            if (!answerSet.contains(path1) && !answerSet.contains(path2)) {
+                answerSet.add(path1);
+                answerSet.add(path2);
+                answer++; // 새로운 경로일 경우 카운트 증가
+            }
+
+            // 현재 위치 업데이트
+            x = newX;
+            y = newY;
         }
 
-        Set<int[]> chk_arr = new HashSet<>();
-        Arrays.stream(load_arr)
-                        .forEach(arr->chk_arr.add(arr));
-        System.out.println(chk_arr.size() );
-
-
-
-        //2-2. 중복되는게 있는지 비교
-        //2-2-1. 중복되는게 있을 경우 중복되는 거 직전의 값을 비교
-        //2-2-2. 직전 값이 같으면 count-1, 다르면 pass
-
-        //3. count 출력
         return answer;
     }
 }
