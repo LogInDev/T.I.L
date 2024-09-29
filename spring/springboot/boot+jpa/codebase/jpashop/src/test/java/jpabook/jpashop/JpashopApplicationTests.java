2b53817aee9652e88b393879f7d1e65d8c058334
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-09-29 19:25:36 +0900

    finished 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발

--- spring/springboot/boot+jpa/codebase/jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ spring/springboot/boot+jpa/codebase/jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,13 +1,25 @@
 package jpabook.jpashop;
 
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JpashopApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
 	@Test
-	void contextLoads() {
+	public void updateTest() throws Exception {
+		Book book = em.find(Book.class, 1L);
+
+		//TX
+		book.setName("asdfasdf");
+
+		//변경감지 == dirty checking
 	}
 
 }
---
+++ spring/springboot/boot+jpa/codebase/jpashop/src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class ItemUpdateTest {
+}
