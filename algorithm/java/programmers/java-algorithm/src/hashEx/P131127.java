1758cd7f8a72e839649610cb17a1e622e9b10563
SaeRom Choi <127198819+LogInDev@users.noreply.github.com>
2024-11-14 08:49:52 +0900

    P131127. 할인행사 - .containsKey(), .allMatch()

---
+++ algorithm/java/programmers/java-algorithm/src/hashEx/P131127.java
@@ -0,0 +1,40 @@
+package hashEx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P131127 {
+    public static void main(String[] args) {
+//        String[] want = {"banana", "apple", "rice", "pork", "pot"};
+        String[] want = {"apple"};
+//        int[] number = {3, 2, 2, 2, 1};
+        int[] number = {10};
+//        String[] discount = {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
+        String[] discount = {"banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"};
+        solution(want, number, discount);
+    }
+    public static int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String, Integer> want_map = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            want_map.put(want[i], number[i]);
+        }
+        int discount_size = discount.length;
+        int st_index = 0;// discount를 순회 시작 index
+        while (st_index <= discount_size - 10) {
+            Map<String, Integer> copy_map = new HashMap<>(want_map);
+            for (int i = st_index; i < 10+st_index; i++) {
+                if (copy_map.containsKey(discount[i])) {
+                    copy_map.put(discount[i], copy_map.get(discount[i]) - 1);
+                }
+            }
+            st_index++;
+            // 사고 싶은 물건들의 남은 합
+            if (copy_map.values().stream().allMatch(value -> value < 1)) {
+                answer++;
+            }
+        }
+//        System.out.println("할인 받을 수 있는 시작 날짜의 개수 = " + answer);
+        return answer;
+    }
+}
