37c86c2e0a9caebf5177a9f55f24128fad4be582
LogInDev <fhazlt303@gmail.com>
2025-01-08 08:38:59 +0900

    P12981. 영어 끝말잇기 - another solution

--- algorithm/java/programmers/java-algorithm/src/set/P12981.java
+++ algorithm/java/programmers/java-algorithm/src/set/P12981.java
@@ -14,39 +14,25 @@ public class P12981 {
         System.out.println("Arrays.toString(result) = " + Arrays.toString(result));
     }
     public static int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        int round = 0;
-        boolean found = true;
-        Set<String> set = new HashSet<>();
-        char sameA = words[0].charAt(0);
-        while(found) {
-            if((n*round)>=words.length-1-n) {found = false;}
-            for (int j = (n*round); j < (n*(round+1)); j++) {
-                if (words[j].length() < 2) {
-                    answer[0] = (j%n)+1;
-                    found = false;
-                    break;
-                }
-                if((j != 0) & (sameA != words[j].charAt(0))) {
-                    answer[0] = (j%n)+1;
-                    found = false;
-                    break;
-                }
-                sameA = words[j].charAt(words[j].length()-1);
-                int bfSetSize = set.size();
-                set.add(words[j]);
-                if (set.size() == bfSetSize) {
-                    answer[0] = (j%n)+1;
-                    found = false;
-                    break;
-                }
 
+        // 이미 사용한 단어를 저자하는 set
+        HashSet<String> usedWords = new HashSet<>();
+        //이전 단어의 마지막 글자, 최초 상태는 첫 번째 사람이 말하는 첫 번째 글자로 초기화
+        char prevWord = words[0].charAt(0);
+
+        for (int i = 0; i < words.length; i++) {
+            //이미 사용한 단어이거나 첫 글자가 이전 단어와 일치하지 않으면
+            if (usedWords.contains(words[i]) || words[i].charAt(0) != prevWord) {
+                //탈락하는 사람의 번호와 차례를 반환
+                return new int[]{(i % n) +1, (i / n) +1};
             }
-            round++;
+            //사용한 단어로 추가
+            usedWords.add(words[i]);
+            //이전 단어의 마지막 글자 업데이트
+            prevWord = words[i].charAt(words[i].length() - 1);
+
         }
-
-        answer[1] = answer[0]==0?0:round;
-
-        return answer;
+        //모두 통과했을 경우 반환값
+        return new int[]{0, 0};
     }
 }
